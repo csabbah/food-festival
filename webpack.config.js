147a26e2ca4a88df886caa58c1d44d09f02b13e9
Csabbah <csabbah@ryerson.ca>
2022-06-22 09:29:31 -0400

    Installed and setup webpack

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+const path = require('path');
+
+module.exports = {
+  entry: './assets/js/script.js', // The root of the bundle and the beginning ofo the dependency graph
+  // webpack will take the above entry point, bundle that cde and 'output' the bundled code to a folder
+  output: {
+    path: path.resolve(__dirname, 'dist'), // Common practice is to put the bundled code in a 'dist' (distribution) folder
+    filename: 'main.bundle.js',
+  },
+  // The mode in which we want the webpack to run
+  // By default, webpack runs in production mode.
+  mode: 'development', // In this mode, webpack will minify our code for us automatically
+};
