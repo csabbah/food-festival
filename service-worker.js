2345e2063abce487312fee8ab71988429ece24bc
Csabbah <csabbah@ryerson.ca>
2022-06-23 09:24:53 -0400

    Added comments in service-workers.js

--- service-worker.js
+++ service-worker.js
@@ -19,7 +19,10 @@ const FILES_TO_CACHE = [
 // service workers run before the window object has been created.
 // So, by using self, we instantiate listeners on the service worker.
 self.addEventListener('install', function (e) {
+  // Wait until the work is complete before terminating the service worker
+  // This ensures the service worker doesn't move from installing phase UNTIL it finished executing all of its code
   e.waitUntil(
+    // We use caches.open to find the specific caches by name, the n add every file frm the FILES_TO_CACHE
     caches.open(CACHE_NAME).then(function (cache) {
       console.log('installing cache : ' + CACHE_NAME);
       return cache.addAll(FILES_TO_CACHE);
